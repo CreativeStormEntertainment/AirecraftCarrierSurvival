a4a38e6c6d4a68fdfa1eb364bf69d2544feae3b4
CreativeStorm <games@creativestormentertainment.com>
2023-01-16 10:39:47 -0700

    - Misc Files

---
+++ Aircraft Carrier Survival/Assets/ArtAssets/AnimatorWaves.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorWaves : MonoBehaviour
+{
+
+    //bool loop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public float speed=1;
+    public float base2 = -2;
+    public float bass3 = 5;
+
+    public float bass4 = 5;
+    float baseLerp = 0;
+    public bool inverted = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (inverted)
+        {
+            if (this.transform.localScale.x <= bass3)
+            {
+                baseLerp = 0;
+            }
+
+            baseLerp += Time.deltaTime / speed;
+            this.transform.localScale = new Vector3(Mathf.Lerp(base2, bass3, baseLerp), Mathf.Lerp(bass4, 0, baseLerp), this.transform.localScale.z);
+        }
+        else
+        {
+            if (this.transform.localScale.x >= bass3)
+            {
+                baseLerp = 0;
+            }
+
+            baseLerp += Time.deltaTime / speed;
+            this.transform.localScale = new Vector3(Mathf.Lerp(base2, bass3, baseLerp), Mathf.Lerp(bass4, 0, baseLerp), this.transform.localScale.z);
+        }
+    }
+}
---
+++ Aircraft Carrier Survival/Assets/ArtAssets/TestMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMover : MonoBehaviour
+{
+    public Transform test;
+    public float speed = 0.1f;
+    public float interval=1.0f;
+    public float testTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testTime+ interval < Time.timeSinceLevelLoad)
+        {
+            testTime = Time.timeSinceLevelLoad;
+            test.gameObject.SetActive(!test.gameObject.activeSelf);
+        }
+        this.transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
